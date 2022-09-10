2c2aacacccd2b65a65b3ef1ecd148efd27cd5eef
umitmutluu <umitmutlurize@gmail.com>
2022-09-10 20:32:01 +0300

    Initial Commit

---
+++ SwiftUISamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISamples
+//
+//  Created by Umut Mutlu on 10.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Umut Mutlu on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
